1f72d453f28d247180474da9f12f12c19460dec3
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-05 19:07:40 +0100

    Update border radious on auth layout

--- vegait-timesheet/app/(auth)/layout.tsx
+++ vegait-timesheet/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import type React from "react";
 export default function layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="bg-grey-100 flex items-center justify-center h-screen">
-			<div className="bg-white w-[392px] h-[252px] rounded-[12px] flex items-center justify-center">
+			<div className="bg-white w-[392px] h-[252px] rounded-[16px] flex items-center justify-center">
 				{children}
 			</div>
 		</div>
