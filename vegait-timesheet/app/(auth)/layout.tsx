6ff1b268ea425c07c791be810cb0ab05901f280e
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-05 17:24:23 +0100

    Add auth pages layout

--- vegait-timesheet/app/(auth)/layout.tsx
+++ vegait-timesheet/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
-import type React from 'react';
+import type React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            {children}
-        </div>
-    );
+	return (
+		<div className="bg-grey-100 flex items-center justify-center h-screen">
+			<div className="bg-white w-[392px] h-[252px] rounded-[12px] flex items-center justify-center">
+				{children}
+			</div>
+		</div>
+	);
 }
