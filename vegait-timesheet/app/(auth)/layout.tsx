4e04ad47f55e3201e40f77da9af455e60b0a5962
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-05 13:53:50 +0100

    Add layout for auth routes

---
+++ vegait-timesheet/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import type React from 'react';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            {children}
+        </div>
+    );
+}
