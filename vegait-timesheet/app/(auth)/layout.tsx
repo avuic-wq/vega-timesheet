6f052240dfc25355fdbb8e8d911099c5e0d9ad51
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-05 20:15:38 +0100

    Add logo to auth layout

--- vegait-timesheet/app/(auth)/layout.tsx
+++ vegait-timesheet/app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
+import { Logo } from "@/components/Logo";
 import type React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="bg-grey-100 flex items-center justify-center h-screen">
+		<div className="bg-grey-100 flex items-center justify-center flex-col h-screen">
+			<Logo spacing={{ bottom: 20 }} />
 			<div className="bg-white w-[392px] h-[252px] rounded-[16px] flex items-center justify-center">
 				{children}
 			</div>
