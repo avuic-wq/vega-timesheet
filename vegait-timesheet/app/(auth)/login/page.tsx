452d6308b636708503063e9929bed801afe843cd
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-06 13:45:47 +0100

    Update type of search params and wrap form in suspense

--- vegait-timesheet/app/(auth)/login/page.tsx
+++ vegait-timesheet/app/(auth)/login/page.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from "react";
 import LoginForm from "@/components/ui/LoginForm";
 
 interface Props {
-	searchParams: { callbackUrl?: string };
+	searchParams: Promise<{ callbackUrl?: string }>;
 }
 
 export default async function LoginPage({ searchParams }: Props) {
 	const params = await searchParams;
-	return <LoginForm callbackUrl={params?.callbackUrl} />;
+	return (
+		<Suspense>
+			<LoginForm callbackUrl={params?.callbackUrl} />
+		</Suspense>
+	);
 }
