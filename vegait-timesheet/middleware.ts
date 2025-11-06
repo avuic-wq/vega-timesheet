7d7f8d0e5ed7313522330d15ef2457ca460071f2
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-06 11:32:20 +0100

    Update middleware

---
+++ vegait-timesheet/middleware.ts
@@ -0,0 +1,16 @@
+export { auth as middleware } from "@/auth/auth";
+
+export const config = {
+	// Protect these routes with authentication
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api/auth (auth API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 * - login (login page)
+		 */
+		"/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)",
+	],
+};
