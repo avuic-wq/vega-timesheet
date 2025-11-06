d6a9de6b1fd7544e14b6f27ebbfbce412832029e
Aleksa <aleksa.vuic@gmail.com>
2025-11-06 21:30:24 +0100

    Rename layout file and Update import

--- vegait-timesheet/src/app/(auth)/layout.tsx
+++ vegait-timesheet/src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react";
-import { Logo } from "@/components/ui/Logo";
+import { Logo } from "@/src/components/ui/Logo";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="bg-grey-100 flex items-center justify-center flex-col h-screen">
 			<Logo className="mb-5" />
