8e26a96cb4f59aa92dad711ce21afbd5962b7a88
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-06 07:55:55 +0100

    Add new prop to button

--- vegait-timesheet/components/ui/Button.tsx
+++ vegait-timesheet/components/ui/Button.tsx
@@ -2,18 +2,21 @@ interface ButtonProps {
 	text: string;
 	type?: "button" | "submit";
 	onClick?: () => void;
+	isDisabled?: boolean;
 }
 
 export default function Button({
 	text,
 	type = "button",
 	onClick,
+	isDisabled = false,
 }: ButtonProps) {
 	return (
 		<button
 			type={type}
 			onClick={onClick}
 			className="bg-black text-white rounded-[24px] w-[200px] h-[40px] p-4 flex items-center justify-center m-auto"
+			disabled={isDisabled}
 		>
 			{text}
 		</button>
