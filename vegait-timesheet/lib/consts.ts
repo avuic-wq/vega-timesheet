7cb1d80a1a2abc95236e7f3a00d71311f2f2676f
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-06 09:48:34 +0100

    Update consts

--- vegait-timesheet/lib/consts.ts
+++ vegait-timesheet/lib/consts.ts
@@ -2,3 +2,6 @@ export const DEFAULT_SALT_ROUNDS = 10
 export const AUTH_PROVIDERS = {
     CREDENTIALS: "credentials"
 }
+export const AUTH_ERRORS = {
+    INVALID_CREDENTIALS: "CredentialsSignin"
+}
