5803b604a0cf2c9016602a5eec07873b8381ab22
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-06 10:48:46 +0100

    Update login page

--- vegait-timesheet/app/(auth)/login/page.tsx
+++ vegait-timesheet/app/(auth)/login/page.tsx
@@ -1,26 +1,7 @@
 "use client";
 
-import { authenticate } from "@/app/actions/auth";
-import Button from "@/components/ui/Button";
-import TextField from "@/components/ui/TextField";
-import { useActionState } from "react";
+import LoginForm from "@/components/ui/LoginForm";
 
 export default function LoginPage() {
-	const [errorMessage, formAction, isPending] = useActionState(
-		authenticate,
-		undefined,
-	);
-
-	return (
-		<form action={formAction}>
-			<TextField name="username" placeholder="Username" error={errorMessage} />
-			<TextField
-				name="password"
-				placeholder="Password"
-				error={errorMessage}
-				isPassword
-			/>
-			<Button text="Login" type="submit" />
-		</form>
-	);
+	return <LoginForm />;
 }
