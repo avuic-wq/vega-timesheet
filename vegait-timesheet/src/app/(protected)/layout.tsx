f5111276720435c6fbfac3bbe9423d82678dd488
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-07 14:56:38 +0100

    Remove unnecessary session check on protected layout

--- vegait-timesheet/src/app/(protected)/layout.tsx
+++ vegait-timesheet/src/app/(protected)/layout.tsx
@@ -1,19 +1,9 @@
-import { redirect } from "next/navigation";
-import { auth } from "@/auth/auth";
 import Navigation from "@/src/components/Navigation";
-import { APP_ROUTES } from "@/src/lib/consts";
-
 export default async function ProtectedLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const session = await auth();
-
-	if (!session) {
-		redirect(APP_ROUTES.LOGIN);
-	}
-
 	return (
 		<>
 			<Navigation />
