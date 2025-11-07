907b47485158632a4cc7d665657f8e1bbbdc23dc
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-07 13:57:10 +0100

    Add initial footer

--- vegait-timesheet/src/components/Footer.tsx
+++ vegait-timesheet/src/components/Footer.tsx
@@ -1,3 +1,19 @@
+import Logo from "@/src/components/Logo";
+import Text from "@/src/components/Text";
+
+// TO-DO: something with these strings
+const footerText =
+	"Co-creators. Passionate innovators. We're your software development partners, working at the cutting edge of digital product development. We have the technical expertise and domain experience to help you push boundaries, regardless of your industry";
+const copyrightText = "© Copyright Vega IT. All rights reserved.";
+
 export default function Footer() {
-	return <div className="bg-white rounded-[16px]">Footer</div>;
+	return (
+		<div className="flex items-center justify-center bg-white rounded-[16px] py-12 pl-70 pr-240">
+			<div className="flex items-left flex-col gap-8">
+				<Logo />
+				<Text value={footerText} />
+				<Text value={copyrightText} />
+			</div>
+		</div>
+	);
 }
