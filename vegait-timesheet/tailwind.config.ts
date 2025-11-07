1fa2d6a20cd03113975268a5534b1a739a13b4c5
Aleksa <aleksa.vuic@gmail.com>
2025-11-07 17:45:51 +0100

    Fix tailwind setup & global css

--- vegait-timesheet/tailwind.config.ts
+++ vegait-timesheet/tailwind.config.ts
@@ -1,16 +1,10 @@
 import type { Config } from "tailwindcss";
-import { colors } from "./src/lib/colors";
 
-export default {
+const config: Config = {
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {
-		extend: {
-			colors,
-		},
-		// TO-DO: screens: { sm: "...px", etc }
-	},
-	plugins: [],
-} satisfies Config;
+};
+
+export default config;
