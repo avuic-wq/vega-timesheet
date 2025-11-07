337e176b479e6de5b881beae6cd4598871af8c61
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-07 08:22:22 +0100

    Remove comment

--- vegait-timesheet/auth/authenticateUser.ts
+++ vegait-timesheet/auth/authenticateUser.ts
@@ -1,8 +1,6 @@
 import { findUserByUsername } from "@/src/lib/services/userService";
 import { validatePassword } from "@/src/lib/utils/validatePassword";
 
-
-// CHECK: is this valid function? In auth folder and calls prisma/db
 export const authenticateUser = async (username: string, password: string) => {
 	const user = await findUserByUsername(username);
 
