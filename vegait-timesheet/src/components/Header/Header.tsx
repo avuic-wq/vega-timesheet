911bfd97855f56aed6134090ad360bf3d4302455
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-07 15:02:39 +0100

    Add action text in header

--- vegait-timesheet/src/components/Header/Header.tsx
+++ vegait-timesheet/src/components/Header/Header.tsx
@@ -10,12 +10,19 @@ export default function Header() {
 	const config = headerConfigs[pathname] || {};
 	const { title, description, actionText, actionHandler } = config;
 	// TO-DO: Header title should be heading - lg
+	// CHECK: Its better to have a hook that returns actionHandlers?
+
 	return (
 		<div className="flex justify-center bg-white rounded-[16px] p-6">
 			<div className="flex-1 flex flex-col gap-6 border-1">
 				<Text value={title} className="font-bold" />
 				<Text value={description} />
-				{actionHandler && <Icon name="plus" onClick={actionHandler} />}
+				{actionHandler && (
+					<div className="flex justify-left align-center gap-2">
+						<Icon name="plus" onClick={actionHandler} />
+						<Text value={actionText} />
+					</div>
+				)}
 			</div>
 
 			<div className="flex-1 border-1">{/* // Search */}</div>
