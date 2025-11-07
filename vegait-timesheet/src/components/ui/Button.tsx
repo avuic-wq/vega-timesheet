630ed844f729385f5cb231cc97e0f46a2cc7da25
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-07 09:23:57 +0100

    Update button default padding

--- vegait-timesheet/src/components/ui/Button.tsx
+++ vegait-timesheet/src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 }
 
 const staticStyles =
-	"bg-black text-white rounded-[24px] pt-3 pb-3 pl-13 pr-13 flex items-center justify-center m-auto";
+	"bg-black text-white rounded-[24px] pt-3 pb-3 pl-9 pr-9 flex items-center justify-center m-auto";
 
 export default function Button({
 	text,
