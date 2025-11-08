fff7c11f685e573bdcd1cd4bdc87765aa9928ed5
Aleksa <aleksa.vuic@gmail.com>
2025-11-08 18:08:39 +0100

    Update button style logic

--- vegait-timesheet/src/components/Button.tsx
+++ vegait-timesheet/src/components/Button.tsx
@@ -2,34 +2,46 @@
 
 import Text from "@/src/components/Text";
 
+type DynamicStyles = {
+	button: string;
+	text: string;
+};
+
 interface ButtonProps {
 	text: string;
 	type?: "button" | "submit";
-	onClick?: () => void;
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
 	isDisabled?: boolean;
-	className?: string;
+	classNames?: DynamicStyles;
 }
 
 const staticStyles =
-	"bg-black text-white rounded-[24px] py-3 px-9 flex items-center justify-center m-auto cursor-pointer";
+	"bg-black text-white rounded-[24px] flex items-center justify-center m-auto cursor-pointer";
 
 export default function Button({
 	text,
 	type = "button",
 	onClick,
 	isDisabled = false,
-	className = "",
+	classNames = { button: "", text: "" },
 }: ButtonProps) {
-	const dynamicStyles = `${className} ${isDisabled && "bg-grey-500"}`;
+	// TO-DO: Refactor Passing CSS classes
+	const hasFixedDimensions =
+		classNames.button?.includes("w-[") && classNames.button?.includes("h-[");
+
+	const dynamicStyles = {
+		button: `${classNames.button} ${isDisabled ? "bg-grey-500" : ""} ${hasFixedDimensions ? "" : "py-3 px-9"}`,
+		text: `${classNames.text}`,
+	};
 
 	return (
 		<button
 			type={type}
 			onClick={onClick}
-			className={`${staticStyles} ${dynamicStyles}`}
+			className={`${staticStyles} ${dynamicStyles.button} `}
 			disabled={isDisabled}
 		>
-			<Text value={text} />
+			<Text value={text} className={dynamicStyles.text} />
 		</button>
 	);
 }
