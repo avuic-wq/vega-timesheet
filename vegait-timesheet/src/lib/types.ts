c7ed1e9e3b8c20c0cc58fc3c9bbbb328864a60c6
Aleksa <aleksa.vuic@gmail.com>
2025-11-08 22:25:09 +0100

    Update types

--- vegait-timesheet/src/lib/types.ts
+++ vegait-timesheet/src/lib/types.ts
@@ -1,9 +1,15 @@
+import type { Client, Project } from "@prisma/client";
 import type { AUTH_ERRORS_KEYS } from "./consts";
 
 export type AuthErrorKey =
 	(typeof AUTH_ERRORS_KEYS)[keyof typeof AUTH_ERRORS_KEYS];
+
+// TO-DO: Type SEARCH_PARAMETERS const with this or?
 export type SearchParams = Promise<{
 	callbackUrl?: string;
 	search?: string;
 	letterFilter?: string;
 }>;
+
+export type ClientType = Client & { __typename: "client" };
+export type ProjectType = Project & { __typename: "project" };
