8db2eef35cbfb326db4159bf75ccce4f5196ad65
Aleksa <aleksa.vuic@gmail.com>
2025-11-10 18:05:18 +0100

    Refactor filters wrapper to be server side and move logic to filter item

--- vegait-timesheet/src/components/Filters/FilterItem.tsx
+++ vegait-timesheet/src/components/Filters/FilterItem.tsx
@@ -1,24 +1,51 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { SEARCH_PARAMETERS } from "@/src/lib/consts";
+
 interface Props {
 	value: string;
-	isSelected: boolean;
-	onClick: React.MouseEventHandler<HTMLDivElement>;
 	isDisabled?: boolean;
 }
 
-const FilterItem = ({
-	value,
-	isSelected,
-	onClick,
-	isDisabled = false,
-}: Props) => {
+const FilterItem = ({ value, isDisabled = false }: Props) => {
+	const searchParams = useSearchParams();
+	const router = useRouter();
+
+	const selectedFilter =
+		searchParams.get(SEARCH_PARAMETERS.LETTER_FILTER) || null;
+	const isSelected = selectedFilter === value;
+
+	// CHECK: Could use useTransition
+	// const [isPending, startTransition] = useTransition();
+	const handleOnSelectFilter = (filter: string): void => {
+		const params = new URLSearchParams(searchParams);
+
+		if (filter === selectedFilter) {
+			params.delete(SEARCH_PARAMETERS.LETTER_FILTER);
+			params.delete(SEARCH_PARAMETERS.PAGE);
+			router.push(`?${params.toString()}`);
+			return;
+		}
+
+		if (filter) {
+			params.set(SEARCH_PARAMETERS.LETTER_FILTER, filter);
+			params.delete(SEARCH_PARAMETERS.PAGE);
+			router.push(`?${params.toString()}`);
+
+			return;
+		}
+
+		params.delete(SEARCH_PARAMETERS.LETTER_FILTER);
+		router.push(`?${params.toString()}`);
+	};
+
+	const onSelectFilter = isDisabled ? undefined : handleOnSelectFilter;
+
 	const dynamicStyles = `
 	${isSelected ? "border-[1.5px] border-red" : ""}
 	${isDisabled ? "!bg-grey-200 !cursor-not-allowed" : ""}
 	`;
-
-	const handleOnClick = isDisabled ? undefined : onClick;
 
 	return (
 		// CHECK: padding or fixed width/height?
@@ -33,7 +60,7 @@ const FilterItem = ({
 		// TO-DO: Change style on hover (!isDisabled & !isSelected) (when tailwind clases are optimized)
 
 		<div
-			onClick={handleOnClick}
+			onClick={(_e) => onSelectFilter(value)}
 			className={`flex justify-center items-center rounded-[16px] bg-primary w-[40px] h-[40px] cursor-pointer ${dynamicStyles}`}
 		>
 			{value}
--- vegait-timesheet/src/components/Filters/LetterFilters.tsx
+++ vegait-timesheet/src/components/Filters/LetterFilters.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useRouter, useSearchParams } from "next/navigation";
-import { SEARCH_PARAMETERS } from "@/src/lib/consts";
 import FilterItem from "./FilterItem";
 
 const allLettersInAlphabet = Array.from({ length: 26 }, (_, i) =>
@@ -13,44 +9,12 @@ interface Props {
 }
 
 const LetterFilters = ({ letters }: Props) => {
-	const searchParams = useSearchParams();
-	const router = useRouter();
-
-	const selectedFilter =
-		searchParams.get(SEARCH_PARAMETERS.LETTER_FILTER) || null;
-
-	// CHECK: Could use useTransition
-	// const [isPending, startTransition] = useTransition();
-	const handleOnSelectFilter = (filter: string): void => {
-		const params = new URLSearchParams(searchParams);
-
-		if (filter === selectedFilter) {
-			params.delete(SEARCH_PARAMETERS.LETTER_FILTER);
-			params.delete(SEARCH_PARAMETERS.PAGE);
-			router.push(`?${params.toString()}`);
-			return;
-		}
-
-		if (filter) {
-			params.set(SEARCH_PARAMETERS.LETTER_FILTER, filter);
-			params.delete(SEARCH_PARAMETERS.PAGE);
-			router.push(`?${params.toString()}`);
-
-			return;
-		}
-
-		params.delete(SEARCH_PARAMETERS.LETTER_FILTER);
-		router.push(`?${params.toString()}`);
-	};
-
 	return (
 		<div className="flex justify-between">
 			{allLettersInAlphabet.map((letter) => (
 				<FilterItem
 					key={letter}
 					value={letter}
-					isSelected={selectedFilter === letter}
-					onClick={() => handleOnSelectFilter(letter)}
 					isDisabled={!letters.includes(letter)}
 				/>
 			))}
