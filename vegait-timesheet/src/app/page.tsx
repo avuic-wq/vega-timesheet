90f6a6ce0940b22140e92fd0892a13b7a501bd63
Aleksa <aleksa.vuic@gmail.com>
2025-11-11 11:24:49 +0100

    Remove redundant redirect logic since we have it in the middleware

--- vegait-timesheet/src/app/page.tsx
+++ vegait-timesheet/src/app/page.tsx
@@ -1,13 +1,1 @@
-import { redirect } from "next/navigation";
-import { auth } from "@/auth/auth";
-import { APP_ROUTES } from "../lib/consts";
-
-export default async function Home() {
-	const session = await auth();
-
-	if (session) {
-		redirect(APP_ROUTES.CLIENTS);
-	}
-
-	redirect(APP_ROUTES.LOGIN);
-}
+export default async function Home() {}
