7663d09ec142d6e00156b5c72d9708670e53cf56
Aleksa <aleksa.vuic@gmail.com>
2025-11-11 11:36:28 +0100

    Remove prop from action button since it already renders conditionally

--- vegait-timesheet/src/components/Header/ActionButton.tsx
+++ vegait-timesheet/src/components/Header/ActionButton.tsx
@@ -7,23 +7,18 @@ import { Modal } from "@/src/components/Modal/Modal";
 import Text from "@/src/components/Text";
 import { config } from "./config";
 
-interface Props {
-	show?: boolean;
-}
-
-const ActionButton = ({ show }: Props) => {
+const ActionButton = () => {
 	const [isActionModalOn, setIsActionModalOn] = useState<boolean>(false);
 	const pathname = usePathname();
 	const { actionText } = config[pathname];
 
 	return (
 		<>
-			{show && (
-				<div className="flex justify-left align-center gap-2 cursor-pointer w-fit">
-					<Icon name="plus" onClick={() => {}} />
-					<Text value={actionText} className="body-md" />
-				</div>
-			)}
+			<div className="flex justify-left align-center gap-2 cursor-pointer w-fit">
+				<Icon name="plus" onClick={() => {}} />
+				<Text value={actionText} className="body-md" />
+			</div>
+
 			<Modal isOpen={isActionModalOn} onClose={() => setIsActionModalOn(false)}>
 				TEST CONTENT
 				{/* <CreateClientForm /> */}
