42e70e15f4eed81b5ca2e6d33b4c0e2e5fe1dc79
Aleksa <aleksa.vuic@gmail.com>
2025-11-11 18:19:01 +0100

    Add modal create client page

---
+++ vegait-timesheet/src/app/(protected)/(with footer)/clients/@modal/(.)create/page.tsx
@@ -0,0 +1,10 @@
+export default async function CreateClientModal() {
+	return (
+		<div>TEST</div>
+		// <ModalWrapper>
+		// 	<Modal isOpen={true} returnUrl={APP_ROUTES.CLIENTS}>
+		// 		<h2>Create existing Client</h2>
+		// 	</Modal>
+		// </ModalWrapper>
+	);
+}
