6cad7b513efed05cc428f627be3bf3400f7460a8
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-12 10:37:42 +0100

    Update filter style

--- vegait-timesheet/src/components/Filters/utils.ts
+++ vegait-timesheet/src/components/Filters/utils.ts
@@ -6,11 +6,18 @@ export const getFilterStyle = (
 ) => {
 	const commonStyle =
 		"flex justify-center items-center rounded-[16px] bg-primary min-w-[40px] min-h-[40px] cursor-pointer";
-	const disabledStyle = "bg-grey-200 cursor-not-allowed";
+
+	const shadowStyle =
+		"shadow-grey-500 hover:shadow-[0_4px_8px_rgba(0,0,0,0.2)] transition-all";
+
+	const disabledStyle =
+		"bg-grey-200 cursor-not-allowed shadow-none hover:shadow-none";
+
 	const selectedStyle = "border-[1.5px] border-red";
 
 	return twMerge(
 		commonStyle,
+		shadowStyle,
 		isDisabled ? disabledStyle : undefined,
 		isSelected ? selectedStyle : undefined,
 	);
