c40e433f2dd5039bb2f79892ccbe2298b66bf848
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-12 10:40:36 +0100

    Update filter style

--- vegait-timesheet/src/components/Filters/utils.ts
+++ vegait-timesheet/src/components/Filters/utils.ts
@@ -7,8 +7,7 @@ export const getFilterStyle = (
 	const commonStyle =
 		"flex justify-center items-center rounded-[16px] bg-primary min-w-[40px] min-h-[40px] cursor-pointer";
 
-	const shadowStyle =
-		"shadow-grey-500 hover:shadow-[0_4px_8px_rgba(0,0,0,0.2)] transition-all";
+	const shadowStyle = `${isSelected ? "shadow-red" : "shadow-grey-500"} hover:shadow-[0_4px_8px_rgba(0,0,0,0.2)] transition-all`;
 
 	const disabledStyle =
 		"bg-grey-200 cursor-not-allowed shadow-none hover:shadow-none";
