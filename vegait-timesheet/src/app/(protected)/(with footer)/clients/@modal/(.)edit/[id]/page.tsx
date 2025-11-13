ee29e04958cdd2ecae2ed3285f59fd1f913a4568
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-13 10:34:05 +0100

    Fix prop

--- vegait-timesheet/src/app/(protected)/(with footer)/clients/@modal/(.)edit/[id]/page.tsx
+++ vegait-timesheet/src/app/(protected)/(with footer)/clients/@modal/(.)edit/[id]/page.tsx
@@ -1,6 +1,8 @@
+import { fetchClientById } from "@/src/app/db/ClientsService/service";
 import EditForm from "@/src/components/EditForm/EditForm";
 import type { FormConfig } from "@/src/components/EditForm/types";
 import Modal from "@/src/components/Modal/Modal";
+import { countriesData } from "@/src/lib/countriesData";
 
 const modalTitle = "Client";
 
@@ -9,32 +11,44 @@ interface Props {
 }
 
 export default async function UpdateClientModal({ params }: Props) {
-	const clientId = params.id;
-	// const { countries: allCountries } = fetchAllCountries()
-	// const { client } = fetchClientById(clientId)
-	// const deleteClient = deleteClientByIdAction(clientId)
-	// CHECK IF THE SNYTAX IS OK FOR ABOVE
+	const { id: clientId } = await params;
 
 	// Promise.all[]
+	// const deleteClient = await deleteClientByIdAction(clientId)
+	const clientData = await fetchClientById(clientId);
 
-	const deleteClient = () => console.log("Deleting client");
-	const updateClient = () => console.log("Updating client");
+	const countriesOptions = Object.values(countriesData).map((countryData) => {
+		return { label: countryData.name, value: countryData.iso3 };
+	});
+
+	const initialCountry = countriesOptions.find((option) => {
+		return option.value === clientData?.countryCode;
+	}) || { label: "", value: "" };
 
 	// TO-DO: Extract
 	const formConfig: FormConfig = {
 		fields: [
-			{ name: "client-name", type: "text", placeholder: "Name", value: "" },
+			{
+				name: "client-name",
+				type: "text",
+				placeholder: "Name",
+				initialValue: clientData?.name || "",
+				isRequired: true,
+			},
 			{
 				name: "client-address",
 				type: "text",
 				placeholder: "Address",
-				value: "",
+				initialValue: clientData?.address || "",
+				isRequired: true,
 			},
 			{
 				name: "client-countryISO",
-				type: "text",
+				type: "select",
 				placeholder: "Country",
-				value: "",
+				initialValue: initialCountry,
+				options: countriesOptions,
+				isRequired: true,
 			},
 		],
 		buttons: [
@@ -42,12 +56,12 @@ export default async function UpdateClientModal({ params }: Props) {
 				text: "Save",
 				variant: "primary",
 				isDisabled: false,
-				onClick: updateClient,
+				action: "UPDATE",
 			},
 			{
 				text: "Delete",
 				variant: "danger",
-				onClick: deleteClient,
+				action: "DELETE",
 			},
 		],
 	};
