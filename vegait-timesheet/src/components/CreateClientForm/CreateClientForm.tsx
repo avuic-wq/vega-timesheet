2dbe6ca80018b6a9477b7ff60a2f90bcdcd6bd12
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-17 14:06:07 +0100

    Add create client form

---
+++ vegait-timesheet/src/components/CreateClientForm/CreateClientForm.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import FormController from "@/src/components/Form/FormController";
+import type { ClientFormData, SelectOption } from "@/src/components/Form/types";
+import { clientFormConfigFactory } from "@/src/lib/Factory/clientFormConfigFactory";
+import { formActionsFactory } from "@/src/lib/Factory/formActionsFactory";
+
+interface Props<T> {
+	countryOptions: SelectOption[];
+}
+
+const CreateClientForm = <T,>({ countryOptions }: Props<T>) => {
+	const formConfig = clientFormConfigFactory({
+		mode: "create",
+		countryOptions,
+	});
+
+	const formActions = formActionsFactory({
+		mode: "create",
+		entityType: "client",
+	});
+
+	const initialValues: ClientFormData = {
+		name: "",
+		address: "",
+		countryCode: "",
+	};
+
+	return (
+		<FormController
+			mode="create"
+			config={formConfig}
+			actions={formActions}
+			initialValues={initialValues}
+		/>
+	);
+};
+
+export default CreateClientForm;
