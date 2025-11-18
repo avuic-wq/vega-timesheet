2604e66f2a48aaafa71b68142d21bc3603380786
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-18 13:15:29 +0100

    Add children to header

--- vegait-timesheet/src/components/Header/Header.tsx
+++ vegait-timesheet/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import type { PropsWithChildren } from "react";
 import Text from "@/src/components/Text/Text";
 import ActionButton from "./ActionButton";
 import { config as headerConfig } from "./config";
@@ -11,28 +12,30 @@ interface Props {
 	setting: string;
 }
 
-const Header = ({ setting }: Props) => {
-	// TO-DO: Might be a wrapper because of report filters
+const Header: React.FC<PropsWithChildren<Props>> = ({ children, setting }) => {
 	const config = headerConfig[setting] || {};
 	const { title, description, hasActionButton, hasSearch } = config;
 
 	const dynamicStyles = `${hasSearch ? "justify-center" : "justify-left"}`;
 
 	return (
-		<div className={`${staticStyles} ${dynamicStyles}`}>
-			<div className="basis-[45%] flex flex-col gap-6">
-				<Text value={title} className="font-bold heading-lg" />
-				<Text value={description} className="text-lg" />
-				{hasActionButton && <ActionButton setting={setting} />}
-			</div>
-
-			{hasSearch && (
-				<div className="basis-[55%] flex justify-end items-center">
-					<div className="basis-[60%] h-fit">
-						<Search />
-					</div>
+		<div>
+			<div className={`${staticStyles} ${dynamicStyles}`}>
+				<div className="basis-[45%] flex flex-col gap-6">
+					<Text value={title} className="font-bold heading-lg" />
+					<Text value={description} className="text-lg" />
+					{hasActionButton && <ActionButton setting={setting} />}
 				</div>
-			)}
+
+				{hasSearch && (
+					<div className="basis-[55%] flex justify-end items-center">
+						<div className="basis-[60%] h-fit">
+							<Search />
+						</div>
+					</div>
+				)}
+			</div>
+			{children}
 		</div>
 	);
 };
