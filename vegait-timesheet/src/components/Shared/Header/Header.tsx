19ef4553f8d6b72d4bf41a2f7228e2e76f9fdc9f
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-19 11:01:57 +0100

    Update header styles

--- vegait-timesheet/src/components/Shared/Header/Header.tsx
+++ vegait-timesheet/src/components/Shared/Header/Header.tsx
@@ -6,21 +6,24 @@ import ActionButton from "./ActionButton";
 import { config as headerConfig } from "./config";
 import Search from "./Search";
 
-const staticStyles = "flex bg-white rounded-2xl p-6 gap-2";
+const wrapperStyles = "flex flex-col bg-white rounded-2xl p-6 gap-10";
 
 interface Props {
 	setting: string;
 }
 
-const Header: React.FC<PropsWithChildren<Props>> = ({ children, setting }) => {
+const Header: React.FC<PropsWithChildren<PropsWithChildren<Props>>> = ({
+	children,
+	setting,
+}) => {
 	const config = headerConfig[setting] || {};
 	const { title, description, hasActionButton, hasSearch } = config;
 
-	const dynamicStyles = `${hasSearch ? "justify-center" : "justify-left"}`;
+	const mainHeaderStyles = `${hasSearch ? "justify-center" : "justify-left"}`;
 
 	return (
-		<div>
-			<div className={`${staticStyles} ${dynamicStyles}`}>
+		<div className={wrapperStyles}>
+			<div className={`flex ${mainHeaderStyles}`}>
 				<div className="basis-[45%] flex flex-col gap-6">
 					<Text value={title} className="font-bold heading-lg" />
 					<Text value={description} className="text-lg" />
