c7673cc1b7f80e44a9b91551b090b67b732611d1
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-20 14:03:49 +0100

    Update text component

--- vegait-timesheet/src/components/Shared/Text/Text.tsx
+++ vegait-timesheet/src/components/Shared/Text/Text.tsx
@@ -1,10 +1,13 @@
+import { twMerge } from "tailwind-merge";
+
 interface Props {
 	value?: string;
 	className?: string;
 }
 
 const Text = ({ value, className }: Props) => {
-	return <p className={className}>{value}</p>;
+	const style = twMerge("hover:cursor-default", className);
+	return <p className={style}>{value}</p>;
 };
 
 export default Text;
