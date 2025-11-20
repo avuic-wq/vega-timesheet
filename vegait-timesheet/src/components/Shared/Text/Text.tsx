5524dcc61bfb00db163f30e6b3f6c3a138c5befd
Aleksa Vuic <a.vuic@vegait.rs>
2025-11-20 14:10:13 +0100

    Revert text changes

--- vegait-timesheet/src/components/Shared/Text/Text.tsx
+++ vegait-timesheet/src/components/Shared/Text/Text.tsx
@@ -1,15 +1,10 @@
-"use client";
-
-import { twMerge } from "tailwind-merge";
-
 interface Props {
 	value?: string;
 	className?: string;
 }
 
 const Text = ({ value, className }: Props) => {
-	const style = twMerge("hover:cursor-default", className);
-	return <p className={style}>{value}</p>;
+	return <p className={className}>{value}</p>;
 };
 
 export default Text;
